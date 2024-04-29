2dbec2703518108405a8ed6342d98586670011d1
andrecasmac <a01254180@tec.mx>
2024-04-28 19:21:37 -0700

    Changed to black background

--- todo-frontend/src/app/tasks/page.js
+++ todo-frontend/src/app/tasks/page.js
@@ -38,17 +38,17 @@ export default function TaskList() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24 " >
       
-    <h3 class="mb-4 font-semibold text-white dark:text-white">Your Tasks</h3>
-    <ul class="text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+    <h3 className="mb-4 font-semibold text-white dark:text-white">Your Tasks</h3>
+    <ul className="text-sm font-medium text-gray-900 bg-white border rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
 
       {tasks.map((task) => (
         <Task taskInfo={task} deleteTask={deleteTask} />
       ))}
 
-      <li class="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
-        <div class="flex items-center ps-3">
-          <input value={newTaskText} onChange={(item) => setNewTaskText(item.target.value)} type="text" id="new-task" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="New Task.." required />
-          <button onClick={addTask} type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mx-2 my-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">+</button>            
+      <li className="w-full rounded-t-lg">
+        <div className="flex items-center ps-3">
+          <input value={newTaskText} onChange={(item) => setNewTaskText(item.target.value)} type="text" id="new-task" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="New Task.." required />
+          <button onClick={addTask} type="button" className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mx-2 my-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">+</button>            
         </div>
       </li>        
     </ul>
--- todo-frontend/src/components/task.jsx
+++ todo-frontend/src/components/task.jsx
@@ -8,19 +8,19 @@ export default function Task({taskInfo, deleteTask}) {
     const handleCheckbox = () => setIsDone(!isDone);
 
     return (
-        <li class="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
-          <div class="flex items-center ps-3">
+        <li className="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
+          <div className="flex items-center ps-3">
             <input 
               checked={isDone}
               onChange={handleCheckbox}
-              id="angular-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+              id="angular-checkbox" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
             />
             <label 
               style={{ textDecoration: isDone ? "line-through" : "none" }}
-              for="angular-checkbox" class="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">{taskInfo.title}
+              htmlFor="angular-checkbox" className="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">{taskInfo.title}
             </label>
-            <button onClick={handleDelete} class="mx-3">
-              <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+            <button onClick={handleDelete} className="mx-3">
+              <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z"/>
               </svg>
             </button>
